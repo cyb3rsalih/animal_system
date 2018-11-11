11634118358ca38961f3ea78a1df33322e4e27c5
Ebru Gulec <ebru@ebrugulec.com>
2018-11-11 19:13:17 +0300

    change missing png path

--- app/models/calf.rb
+++ app/models/calf.rb
@@ -2,6 +2,6 @@ class Calf < ApplicationRecord
     belongs_to :animal
 
     has_attached_file :image, styles: { medium: "300x300", small: "200x200" },
-                    default_url: "/assets/:style/missing.png"
+                    default_url: "/assets/images/:style/missing.png"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
