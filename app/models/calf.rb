8959503f8891273d170e6024d5ce6f2a261c834c
Ebru Gulec <ebru@ebrugulec.com>
2018-11-18 17:59:15 +0300

    region

--- app/models/calf.rb
+++ app/models/calf.rb
@@ -7,7 +7,7 @@ class Calf < ApplicationRecord
             secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
             s3_region: ENV.fetch('AWS_REGION'),
             bucket: ENV.fetch('S3_BUCKET_NAME'),
-            s3_host_name: "s3-#{ENV['AWS_REGION']}.amazonaws.com"
+            s3_host_name: "s3-#{ENV.fetch('AWS_REGION')}.amazonaws.com"
         },
         path: ":attachment/:id/:style/:filename",
         url: "https://s3.us-east-2.amazonaws.com",
