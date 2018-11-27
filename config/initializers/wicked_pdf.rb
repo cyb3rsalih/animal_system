19a73edd460b1196556fde9505c4cb211c0262ca
Ebru Gulec <ebru@ebrugulec.com>
2018-11-27 20:01:00 +0300

    amd

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,7 +8,8 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.config ||= {}
-WickedPdf.config.merge!({
-  # your extra configurations here
-})
+if Rails.env.staging? || Rails.env.production?
+    exe_path = Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s
+else
+    exe_path = Rails.root.join('bin', 'wkhtmltopdf').to_s
+end
